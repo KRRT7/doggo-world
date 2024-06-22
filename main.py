9f2a6e310286fb04d53cbc31b71f28c1ec579cf5
KRRT7 <106575910+KRRT7@users.noreply.github.com>
2024-06-22 19:44:31 +0000

    test

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from requests import get
 from random import randint
 from numpy import ndarray
 from rich import print
+from pathlib import Path
 if __name__ == "__main__":
     if "__compiled__" in globals():
         print("Running as compiled")
@@ -11,5 +12,7 @@ if __name__ == "__main__":
             resps.append(get("https://www.google.com").status_code)
         print(resps)
         print(array)
+        for path in Path(".").iterdir():
+            print(path)
     else:
         print("Running as script")
