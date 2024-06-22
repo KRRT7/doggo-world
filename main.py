d58eaef406cdbe06792b783e29281a36266c8e83
KRRT7 <106575910+KRRT7@users.noreply.github.com>
2024-06-22 14:44:10 +0000

    yapping

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from requests import get
+from random import randint
+
 if __name__ == "__main__":
     if "__compiled__" in globals():
         print("Running as compiled")
+        for i in range(randint(1,10)):
+            print(get("https://www.google.com").status_code)
     else:
         print("Running as script")
