b5aee7bd4858cb5ebb34d7e79add8dfa6a26a355
KRRT7 <106575910+KRRT7@users.noreply.github.com>
2024-06-22 15:23:27 +0000

    numpy

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from requests import get
 from random import randint
-
+from numpy import ndarray
+from rich import print
 if __name__ == "__main__":
     if "__compiled__" in globals():
         print("Running as compiled")
+        array = ndarray((1000,1000))
+        resps = []
         for i in range(randint(1,10)):
-            print(get("https://www.google.com").status_code)
+            resps.append(get("https://www.google.com").status_code)
+        print(resps)
+        print(array)
     else:
         print("Running as script")
