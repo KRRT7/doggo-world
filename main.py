c14a8dbf9706a96283db50feb2b82f0980dbf9e5
KRRT7 <106575910+KRRT7@users.noreply.github.com>
2024-06-27 15:44:12 +0000

    tms

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from requests import get
-from random import randint
-from numpy import ndarray
-from rich import print
-from pathlib import Path
-if __name__ == "__main__":
-    if "__compiled__" in globals():
-        print("Running as compiled")
-        array = ndarray((1000,1000))
-        resps = []
-        for i in range(randint(1,10)):
-            resps.append(get("https://www.google.com").status_code)
-        print(resps)
-        print(array)
-        for path in Path(".").iterdir():
-            print(path)
-    else:
-        print("Running as script")
+import tsdownsample
+
+if __name__ == '__main__':
+    print("hello world")
